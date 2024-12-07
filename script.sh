f35c4971c2c7c50e43a55548b8fb34c8214f92ae
Cam <rodriguezcm318@gmail.com>
2024-12-07 16:46:56 -0600

    adding hazmat scraper

---
+++ script.sh
@@ -0,0 +1,8 @@
+# goals
+# 1. download TK link
+# 2. download it every week
+# 3. save to repo
+
+# it's NOOOOOT that complicated! :) 
+
+wget "https://public.iema.state.il.us/FOIAHazmatSearch/WeeklyDownload/Hazmat-Reports.csv"
